f5f1f0f3171f0a3fbcae7ddedaf351841b8f101b
userlg <guipeluis90@gmail.com>
2021-04-20 07:35:39 -0400

    New changes UI

---
+++ app.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    newElement: "",
+    element: [
+      { tarea: "Hacer deporte", completed: true },
+      { tarea: "Cocinar la cena", completed: true },
+      { tarea: "Ver tv", completed: false },
+      { tarea: "Practicar Javascript", completed: true },
+    ],
+  },
+  methods: {
+    addElement() {
+      if (this.newElement.length > 1)
+        this.element.push({
+          tarea: this.newElement,
+          completed: false,
+        });
+      this.num = 0;
+      this.newElement = "";
+    },
+    removeElement(num) {
+      if (this.element.length > 0) {
+        this.element.splice(num, 1);
+      }
+    },
+    editElement(num) {
+      if (this.newElement.length > 0) {
+        this.element[num].tarea = this.newElement;
+        this.newElement = "";
+      } else alert("Para editar escriba una actividad en el campo de entrada");
+    },
+    exportPDF() {
+      var vm = this.element;
+      var columns = [
+        { title: "Tarea", dataKey: "tarea" },
+        { title: "Completada", dataKey: "completed" },
+      ];
+      var doc = new jsPDF("p", "pt");
+      // doc.text("Lista de actividades", 40, 40);
+      doc.autoTable(columns, vm);
+      doc.save("file.pdf");
+    },
+  },
+});
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Aplicacion de Prueba con Bootstrap y Vuejs
+
+Aplicacion web para generar lista de actividades en pdf , generadas por los usuarios
+
+
+
+          
+
